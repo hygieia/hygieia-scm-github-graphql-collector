f42caef132769ac032544e018594c3da639f7dc8
Benjamin Xu <jiyang.xu@capitalone.com>
2019-11-26 12:26:08 -0500

    Expands imports of java.util.*

--- src/main/java/com/capitalone/dashboard/collector/GitHubCollectorTask.java
+++ src/main/java/com/capitalone/dashboard/collector/GitHubCollectorTask.java
@@ -33,7 +33,18 @@ import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestClientException;
 
 import java.net.MalformedURLException;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
