3ebd70f087e917027169612e4e96c9057fbe84b0
dcanar9 <anthony.canar@hotmail.com>
2022-06-27 10:10:46 -0700

    added endpoint to fix gitRequests with the wrong collectorItemId and added logging update

--- src/main/java/com/capitalone/dashboard/service/GitHubService.java
+++ src/main/java/com/capitalone/dashboard/service/GitHubService.java
@@ -4,4 +4,6 @@ import org.springframework.http.ResponseEntity;
 
 public interface GitHubService {
     ResponseEntity<String> cleanup();
+
+    ResponseEntity<String> syncPullRequest(String title, String repo, String branch, int limit);
 }
